d4eb48d35ade6cd1b162e64d2314c49c8983fae6
raguay <raguay@customct.com>
2021-05-03 14:46:49 +0700

    :sparkles: Directory visited history and quick save buffers
    You can now revisit directories previously visited. There are also 5 directory memory locations to save a directory and jump back to that directory. It also has a fav keyboard mode.

--- README.md
+++ README.md
@@ -5,8 +5,9 @@ This is an extension to assign favorite folder locations to go to quickly in the
 - Create/Delete Favorite directory locations with assigned names
 - Create/Delete Aliases to often used directories that are shared between computer systems with assigned names.
 - Go to a favorite directory based on assigned name.
+- A favorite directory keyboard mode called `fav` and is switched into the 'fav' mode by 'normal' mode 'f'.
+- `q`, `w`, `e`, `r`, `t` go to the 1st through 5th saved directory in the 'fav' mode and then switches to the 'normal' mode.
+- `Q`, `W`, `E`, `R`, `T` sets the 1st through 5th saved directory in the 'fav' mode and then switches to the 'normal' mode.
+- A directory history with the ability to jump to the past visited directory. No key assignments are made for it. But, you can go to them from the command prompt. The commands are `Backup to x Directory` where x is 1 through 5.
 
-Features in the works:
-
-- Directory push/pop stack with hotkeys to quickly go to a past saved directory.
 
--- main.js
+++ main.js
@@ -14,8 +14,139 @@ var favorites = {
     addCommand('Create Alias Directory', 'favorites.createAlias','Create a directory Alias', favorites.createAlias);
     addCommand('Delete Favorite Directory','favorites.deleteFavorite','Deletes a favorite directory assignment.', favorites.deleteFavorite);
     addCommand('Delete Directory Alias','favorites.deleteAlias','Deletes a favorite directory alias.', favorites.deleteAlias);
+    addCommand('Backup One Directory', 'favorites.popDir1', 'Go back one directory.', () => {
+      favorites.popDir(1);
+    });
+    addCommand('Backup Two Directory', 'favorites.popDir2', 'Go back one directories.', () => {
+      favorites.popDir(2);
+    });
+    addCommand('Backup Three Directory', 'favorites.popDir3', 'Go back one directories.', () => {
+      favorites.popDir(3);
+    });
+    addCommand('Backup Four Directory', 'favorites.popDir4', 'Go back one directories.', () => {
+      favorites.popDir(4);
+    });
+    addCommand('Backup Five Directory', 'favorites.popDir5', 'Go back five directories.', () => {
+      favorites.popDir(5);
+    });
+    addCommand('Save Directory to 1', 'favorites.saveDir1', 'Save to the first directory location.', () => {
+      favorites.saveDir(1);
+    });
+    addCommand('Save Directory to 2', 'favorites.saveDir2', 'Save to the second directory location.', () => {
+      favorites.saveDir(2);
+    });
+    addCommand('Save Directory to 3', 'favorites.saveDir3', 'Save to the third directory location.', () => {
+      favorites.saveDir(3);
+    });
+    addCommand('Save Directory to 4', 'favorites.saveDir4', 'Save to the forth directory location.', () => {
+      favorites.saveDir(4);
+    });
+    addCommand('Save Directory to 5', 'favorites.saveDir5', 'Save to the fifth directory location.', () => {
+      favorites.saveDir(5);
+    });
+    addCommand('Go to Directory 1', 'favorites.saveDir1', 'Save to the first directory location.', () => {
+      favorites.goToSavedDir(1);
+    });
+    addCommand('Go to Directory 2', 'favorites.saveDir2', 'Save to the second directory location.', () => {
+      favorites.goToSavedDir(2);
+    });
+    addCommand('Go to Directory 3', 'favorites.saveDir3', 'Save to the third directory location.', () => {
+      favorites.goToSavedDir(3);
+    });
+    addCommand('Go to Directory 4', 'favorites.saveDir4', 'Save to the forth directory location.', () => {
+      favorites.goToSavedDir(4);
+    });
+    addCommand('Go to Directory 5', 'favorites.saveDir5', 'Save to the fifth directory location.', () => {
+      favorites.goToSavedDir(5);
+    });
+    addCommand('Set Favorite Directory Mode', 'favorites.setFavMode', 'Set the current mode to Favorite Directory mode with abriviation of fav.', favorites.setFavMode);
+
+    //
+    // Add a listener for directory changes.
+    //
+    favorites.extMan.getExtCommand('addDirectoryListener').command(favorites.dirListener);
   },
   installKeyMaps: function() {
+    var newKeyboard = favorites.extMan.getExtCommand('addKeyboardShort').command;
+    favorites.extMan.getExtCommand('createNewMode').command('fav','blue');
+    newKeyboard('normal', false, false, false, 'f', favorites.setFavMode);
+    newKeyboard('fav',false,false,false,'q', () => {
+      favorites.goToSavedDir(1);
+      favorites.extMan.getExtCommand('changeMode').command('normal');
+    });
+    newKeyboard('fav',false,false,false,'w', () => {
+      favorites.goToSavedDir(2);
+      favorites.extMan.getExtCommand('changeMode').command('normal');
+    });
+    newKeyboard('fav',false,false,false,'e', () => {
+      favorites.goToSavedDir(3);
+      favorites.extMan.getExtCommand('changeMode').command('normal');
+    });
+    newKeyboard('fav',false,false,false,'r', () => {
+      favorites.goToSavedDir(4);
+      favorites.extMan.getExtCommand('changeMode').command('normal');
+    });
+    newKeyboard('fav',false,false,false,'t', () => {
+      favorites.goToSavedDir(5);
+      favorites.extMan.getExtCommand('changeMode').command('normal');
+    });
+    newKeyboard('fav',false,true,false,'Q', () => {
+      favorites.saveDir(1);
+      favorites.extMan.getExtCommand('changeMode').command('normal');
+    });
+    newKeyboard('fav',false,true,false,'W', () => {
+      favorites.saveDir(2);
+      favorites.extMan.getExtCommand('changeMode').command('normal');
+    });
+    newKeyboard('fav',false,true,false,'E', () => {
+      favorites.saveDir(3);
+      favorites.extMan.getExtCommand('changeMode').command('normal');
+    });
+    newKeyboard('fav',false,true,false,'R', () => {
+      favorites.saveDir(4);
+      favorites.extMan.getExtCommand('changeMode').command('normal');
+    });
+    newKeyboard('fav',false,true,false,'T', () => {
+      favorites.saveDir(5);
+      favorites.extMan.getExtCommand('changeMode').command('normal');
+    });
+  },
+  setFavMode: function() {
+    favorites.extMan.getExtCommand('changeMode').command('fav');
+  },
+  dirRing: ['','','','','','','','','',''],
+  dirRingIndex: 0,
+  dirListener: function(newPath) {
+    if(!favorites.dirRing[favorites.dirRingIndex].includes(newPath.path)) {
+      favorites.dirRingIndex = (favorites.dirRingIndex + 1) % favorites.dirRing.length;
+      favorites.dirRing[favorites.dirRingIndex] = newPath.path;
+    }
+  },
+  popDir: function(index) {
+    var nInd = (favorites.dirRingIndex - (1+index));
+    if(nInd < 0) nInd = nInd + favorites.dirRing.length;
+    favorites.extMan.getExtCommand('changeDir').command({
+      path: favorites.dirRing[nInd]
+    });
+  },
+  dirSaved: ['','','','',''],
+  saveDir: function(inx) {
+    //
+    // Get the cursor
+    //
+    var cursor = favorites.extMan.getExtCommand('getCursor').command();
+    
+    //
+    // Save the directory.
+    //
+    favorites.dirSaved[inx] = cursor.entry.dir;
+  },
+  goToSavedDir: function(inx) {
+    if(favorites.dirSaved[inx] !== '') {
+      favorites.extMan.getExtCommand('changeDir').command({
+        path: favorites.dirSaved[inx]
+      });
+    }
   },
   getFavDirFile: function() {
     var fdir = favorites.fs.appendPath(favorites.fs.getHomeDir(),'.favoritedirs');
